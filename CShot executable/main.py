d8ef316736f7dd78145a6e5de98114e83328bcd7
Amir <manamir4179@gmail.com>
2025-03-24 20:49:33 +0330

    Add files via upload

---
+++ CShot executable/main.py
@@ -0,0 +1,474 @@
+import math
+import random
+from pygame import mixer
+import pygame, sys, subprocess
+import pygame.freetype
+
+users = sys.argv[1:]
+
+
+class Colors:
+    # Helper class for colors
+    white = (255, 255, 255)
+    gray = (168, 168, 168)
+    dark_gray = (36, 36, 36)
+    muted_dark_gray = (76, 76, 76)
+    muted_gray = (124, 124, 124)
+    muted_red = (150, 50, 50)
+    muted_green = (50, 150, 50)
+    muted_blue = (50, 50, 150)
+    cyan = (0, 150, 199)
+
+
+class Texts:
+    # Helper class for displaying Texts
+    def __init__(self, text, posX, posY, color, font_size):
+        self.text = text
+        self.color = color
+        self.posX = posX
+        self.posY = posY
+        self.size = font_size
+
+    def displayText(self):
+        height, width = self.posY, self.posX
+        text_font = pygame.font.Font('assets/PressStart2P-Regular.ttf', self.size)
+        text_surface = text_font.render(self.text, True, self.color)
+        text_rect = text_surface.get_rect(center=(width, height))
+        screen.blit(text_surface, text_rect)
+
+
+class Buttons:
+    # Helper class for displaying Buttons
+    def __init__(self, text, posX, posY, width, height, background_color, hover_color, text_color, text_size,
+                 action=None):
+        self.text = text
+        self.posX = posX
+        self.posY = posY
+        self.width = width
+        self.height = height
+        self.background_color = background_color
+        self.text_color = text_color
+        self.h_color = hover_color
+        self.font_size = text_size
+        self.action = action
+
+    def displayButton(self):
+        content = Texts(self.text, self.posX, self.posY, self.text_color, self.font_size)
+        width = self.width
+        height = self.height
+        x = self.posX - width // 2
+        y = self.posY - height // 2
+        h_color = self.h_color
+        background_color = self.background_color
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if (x < mouse[0] < x + width and y < mouse[1] < y + height):
+            pygame.draw.rect(screen, h_color, (x, y, width, height), border_radius=2)
+            if click[0] == 1 and self.action:
+                self.action()
+        else:
+            pygame.draw.rect(screen, background_color, (x, y, width, height), border_radius=2)
+
+        content.displayText()
+
+
+class Images:
+    def __init__(self, size, posX, posY, src):
+        self.size = size
+        self.posX = posX
+        self.posY = posY
+        self.src = f"assets/{src}"
+
+    def displayImage(self):
+        emoji_img = pygame.image.load(self.src)
+        emoji_img = pygame.transform.smoothscale(emoji_img, (self.size, self.size))
+        screen.blit(emoji_img, (self.posX, self.posY))
+
+
+class Player:
+    def __init__(self, name='NO_NAME', score=0, bullets=20, time=60):
+        self.name = name[0].upper() + name[1:]
+        self.score = score
+        self.bullets = bullets
+        self.posX = random.randint(100, 1200)
+        self.posY = random.randint(250, 650)
+        self.old_posX = self.posX
+        self.old_posY = self.posY
+        self.bulletHoles = []
+        self.time = time
+        self.extra_time = 0
+        # ////////////////// ADDITIONAL POINTS FOR CONSECUTIVE SUCCESSFUL SHOTS //////////////////
+        self.successful_shot = False
+
+    def moveUp(self):
+        # DONT GO OVER UI
+        if (self.posY > 220):
+            self.posY -= 10
+
+    def moveDown(self):
+        if (self.posY < 665):
+            self.posY += 10
+
+    def moveLeft(self):
+        if (self.posX > 45):
+            self.posX -= 10
+
+    def moveRight(self):
+        if (self.posX < 1225):
+            self.posX += 10
+
+    def shoot(self, player):
+        targets = [target1, target2, target3, ammo1, ammo2, extra_time1, bomb1, bolt1]
+        if (self.bullets > 0):
+            play_sound_effect('shot.mp3', 0.2)
+            self.bullets -= 1
+            if (player == 1):
+                bullet_hole = Images(8, self.posX, self.posY, 'bulletholered.png')
+                self.bulletHoles.append(bullet_hole)
+                self.checkHit(targets)
+            elif (player == 2):
+                bullet_hole = Images(8, self.posX, self.posY, 'bulletholeblue.png')
+                self.bulletHoles.append(bullet_hole)
+                self.checkHit(targets)
+            self.old_posX = self.posX
+            self.old_posY = self.posY
+        else:
+            play_sound_effect('no_bullet.mp3', 0.5)
+
+    def checkHit(self, targets):
+        for target in targets:
+            # ///////////////////////// HIT /////////////////////////
+            if (target.posX - 6 < self.posX < target.posX + 40 and target.posY - 6 < self.posY < target.posY + 40):
+                if (target.__class__.__name__ == 'Ammo'):
+                    play_sound_effect('add_bullet.mp3', 0.5)
+                    target.reset()
+                    self.bullets += 15
+                elif (target.__class__.__name__ == 'Time'):
+                    play_sound_effect('powerup.mp3', 0.5)
+                    target.reset()
+                    self.extra_time += 10
+                elif (target.__class__.__name__ == 'Bomb'):
+                    play_sound_effect('bomb.mp3', 0.5)
+                    bomb1.effect()
+                    target.reset()
+                    self.score -= 3
+                elif (target.__class__.__name__ == 'Bolt'):
+                    global thunder_hit
+                    play_sound_effect('bolt.mp3', 0.8)
+                    thunder_hit = True
+                    bolt1.effect()
+                    target1.reset()
+                    target2.reset()
+                    target3.reset()
+                    target.reset()
+                    self.score += 3
+                # ////////////////////////// MAIN TARGETS //////////////////////////
+                else:
+                    play_sound_effect('powerup.mp3', 0.5)
+                    self.score += self.calScore()
+                    self.successful_shot = True
+                    target.reset()
+                return True
+        self.successful_shot = False
+
+    def calScore(self):
+        distance = math.sqrt((self.posX - self.old_posX) ** 2 + (self.posY - self.old_posY) ** 2)
+        if (self.successful_shot):
+            return int((distance // 100 + 1)) + 3
+        else:
+            return int((distance // 100 + 1))
+
+
+class Target:
+    def __init__(self):
+        self.reset()
+
+    def displayTarget(self):
+        target = Images(36, self.posX, self.posY, 'target.png')
+        target.displayImage()
+
+    def reset(self):
+        self.posX = random.randint(150, 1150)
+        self.posY = random.randint(300, 590)
+
+
+class Ammo(Target):
+    def __init__(self):
+        super().__init__()
+
+    def displayTarget(self):
+        target = Images(36, self.posX, self.posY, 'ammo.png')
+        target.displayImage()
+
+
+class Time(Target):
+    def __init__(self):
+        super().__init__()
+
+    def displayTarget(self):
+        target = Images(36, self.posX, self.posY, 'time.png')
+        target.displayImage()
+
+
+class Bomb(Target):
+    def __init__(self):
+        super().__init__()
+
+    def displayTarget(self):
+        target = Images(36, self.posX, self.posY, 'bomb.png')
+        target.displayImage()
+
+    def effect(self):
+        explosion = Images(128, self.posX - 46, self.posY - 46, 'explosion.png')
+        explosion.displayImage()
+
+
+class Bolt(Target):
+    def __init__(self):
+        super().__init__()
+
+    def displayTarget(self):
+        target = Images(48, self.posX, self.posY, 'lightningBolt.png')
+        target.displayImage()
+
+    def reset(self):
+        self.posX = random.randint(100, 1200)
+        self.posY = random.randint(250, 650)
+
+    def effect(self):
+        targets = [target1, target2, target3]
+
+        for target in targets:
+
+            dots = [(target.posX + 18, 215)]
+            y = 215
+            while y < target.posY + 18:
+
+                y += random.randint(10, 30)
+                if y > target.posY + 18:
+                    y = target.posY + 18
+                dots.append((target.posX + 18 + random.randint(-10, 10), y))
+
+            pygame.draw.lines(screen, Colors.cyan, False, dots, 12)
+            pygame.draw.lines(screen, Colors.white, False, dots, 8)
+
+
+class Actions:
+    @staticmethod
+    def exit_game():
+        subprocess.Popen(["python", "menu.py"])
+        pygame.quit()
+        sys.exit()
+
+
+def play_sound_once(src):
+    sound = pygame.mixer.Sound(f"assets/{src}")
+    global end_game_sound_played
+    if not end_game_sound_played:
+        mixer.music.stop()
+        sound.play()
+        end_game_sound_played = True
+
+
+def play_sound_effect(src, vol):
+    sound = pygame.mixer.Sound(f'assets/{src}')
+    sound.set_volume(vol)
+    sound.play()
+
+
+def display_GUI_STATIC():
+    player1_text = Texts(f'{player1.name}'.ljust(8), 145, 80, Colors.muted_red, 20)
+    player1_text.displayText()
+    player1_emoji = Images(36, 18, 60, 'player1.png')
+    player1_emoji.displayImage()
+    player2_text = Texts(f'{player2.name}'.ljust(8), WIDTH - 140, 80, Colors.muted_blue, 20)
+    player2_text.displayText()
+    player2_emoji = Images(36, WIDTH - 268, 60, 'player2.png')
+    player2_emoji.displayImage()
+
+
+def display_GUI_UPDATE(p1_bullet_count=20, p1_score=0, p2_bullet_count=20, p2_score=0):
+    player1_timer = Texts(f"Timer : {int(player1.time)}s", 165, 30, Colors.white, 18)
+    player1_timer.displayText()
+    timer_emoji = Images(36, 18, 10, 'stopwatch.png')
+    timer_emoji.displayImage()
+    player2_timer = Texts(f"Timer : {int(player2.time)}s", WIDTH - 115, 30, Colors.white, 18)
+    player2_timer.displayText()
+    timer_emoji = Images(36, WIDTH - 268, 10, 'stopwatch.png')
+    timer_emoji.displayImage()
+    # ///////////////////////////////////// PLAYER 1 GUI /////////////////////////////////////
+    player1_bullets = Texts('Bullets', 105, 123, Colors.muted_gray, 14)
+    player1_bullets.displayText()
+    player1_bullets_count = Texts(f"{p1_bullet_count}", 185, 123, Colors.muted_gray, 14)
+    player1_bullets_count.displayText()
+    player1_bullets_emoji = Images(24, 25, 110, 'bullet.png')
+    player1_bullets_emoji.displayImage()
+    player1_score = Texts('Score', 90, 163, Colors.muted_gray, 14)
+    player1_score.displayText()
+    player1_score_count = Texts(f"{p1_score}", 185, 163, Colors.muted_gray, 14)
+    player1_score_count.displayText()
+    player1_score_emoji = Images(24, 24, 150, 'score.png')
+    player1_score_emoji.displayImage()
+    # ///////////////////////////////////// PLAYER 2 GUI /////////////////////////////////////
+    player2_bullets = Texts('Bullets', WIDTH - 177, 123, Colors.muted_gray, 14)
+    player2_bullets.displayText()
+    player2_bullets_count = Texts(f"{p2_bullet_count}", WIDTH - 100, 123, Colors.muted_gray, 14)
+    player2_bullets_count.displayText()
+    player2_bullets_emoji = Images(24, WIDTH - 260, 110, 'bullet.png')
+    player2_bullets_emoji.displayImage()
+    player2_score = Texts('Score', WIDTH - 190, 163, Colors.muted_gray, 14)
+    player2_score.displayText()
+    player2_score_count = Texts(f"{p2_score}", WIDTH - 100, 163, Colors.muted_gray, 14)
+    player2_score_count.displayText()
+    player2_score_emoji = Images(24, WIDTH - 260, 150, 'score.png')
+    player2_score_emoji.displayImage()
+
+
+def display_gameOver_screen(p1_score, p2_score):
+    screen.fill(Colors.dark_gray)
+    go_text = Texts("The Game is Over!", WIDTH // 2, HEIGHT // 4, Colors.white, 40)
+    go_text.displayText()
+
+    p1_final_score = Texts(f"{player1.name}'s Score: {p1_score}", WIDTH // 2, HEIGHT // 2 - 50, Colors.muted_red, 20)
+    p1_final_score.displayText()
+    p2_final_score = Texts(f"{player2.name}'s Score: {p2_score}", WIDTH // 2, HEIGHT // 2 + 50, Colors.muted_blue, 20)
+    p2_final_score.displayText()
+    # ////Winning exception and condition
+    if p1_score > p2_score:
+        Win_text = Texts(f"{player1.name} Wins! Brutal.", WIDTH // 2, HEIGHT // 2 + 150, Colors.muted_red, 25)
+        Win_text.displayText()
+        play_sound_once('win.sf.mp3')
+    elif p2_score > p1_score:
+        Win_text = Texts(f"{player2.name} Wins! Brutal", WIDTH // 2, HEIGHT // 2 + 150, Colors.muted_blue, 25)
+        Win_text.displayText()
+        play_sound_once('win.sf.mp3')
+    else:
+        Win_text = Texts("Draw!", WIDTH // 2, HEIGHT // 2 + 150, Colors.white, 25)
+        Win_text.displayText()
+        play_sound_once('draw.sf.mp3')
+
+    exit_button = Buttons("Exit", WIDTH // 2, HEIGHT - 100, 200, 50, Colors.muted_gray, Colors.gray, Colors.white, 20,
+                          Actions.exit_game)
+    exit_button.displayButton()
+
+
+if __name__ == "__main__":
+    # //////////////////////////////////////////// INITIALIZE PYGAME ////////////////////////////////////////////
+    pygame.init()
+    pygame.display.set_caption('CShot')
+    pygame.font.init()
+    icon = pygame.image.load('assets/icon.jpg')
+    pygame.display.set_icon(icon)
+    pygame.key.set_repeat(500, 50)
+    font = pygame.font.Font('assets/PressStart2P-Regular.ttf', 50)
+    WIDTH, HEIGHT = 1280, 720
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    start_time = pygame.time.get_ticks()
+    bg_img = pygame.image.load('assets/bg_game.png')
+    # //////////////////////////////////////////// INITIALIZE MIXER ////////////////////////////////////////////
+    mixer.music.load('assets/game.sf.mp3')
+    mixer.music.set_volume(0.3)
+    mixer.music.play(-1)
+    # //////////////////////////////////////////// INITIALIZE OBJECTS ////////////////////////////////////////////
+    end_game_sound_played = False
+    thunder_hit = False
+    player1 = Player(users[0])
+    player2 = Player(users[1])
+
+    target1 = Target()
+    target2 = Target()
+    target3 = Target()
+
+    ammo1 = Ammo()
+    ammo2 = Ammo()
+    extra_time1 = Time()
+    bomb1 = Bomb()
+    bolt1 = Bolt()
+    # //////////////////////////////////////////// CREATE CUSTOM EVENTS ////////////////////////////////////////////
+    ammo_spawn = pygame.USEREVENT + 1
+    time_spawn = pygame.USEREVENT + 2
+    bomb_spawn = pygame.USEREVENT + 3
+    pygame.time.set_timer(ammo_spawn, 10000)
+    pygame.time.set_timer(time_spawn, 5000)
+    pygame.time.set_timer(bomb_spawn, 15000)
+    running = True
+    # //////////////////////////////////////////// MAIN DRIVER CODE ////////////////////////////////////////////
+    while running:
+        clock.tick(90)
+        screen.blit(bg_img, (0, 0))
+        # //////////////////////////////////////////// INITIALIZE TIMER ////////////////////////////////////////////
+        e_time = 60 - (pygame.time.get_ticks() - start_time) // 1000
+        player1.time = e_time + player1.extra_time if e_time + player1.extra_time > 0 else 0
+        player2.time = e_time + player2.extra_time if e_time + player2.extra_time > 0 else 0
+        # //////////////////////////////////////////// CHECK END GAME CONDITIONS ////////////////////////////////////////////
+        if (player1.bullets == 0 and player2.bullets == 0) or (player1.time == 0 and player2.time == 0) or (
+                player1.time == 0 and player2.bullets == 0) or (player1.bullets == 0 and player2.time == 0):
+            display_gameOver_screen(player1.score, player2.score)
+        else:
+            # //////////////////////////////////////////// DISPLAY TARGETS ////////////////////////////////////////////
+            target1.displayTarget()
+            target2.displayTarget()
+            target3.displayTarget()
+            extra_time1.displayTarget()
+            ammo1.displayTarget()
+            ammo2.displayTarget()
+            bomb1.displayTarget()
+            if (e_time < 35 and e_time > 15 and thunder_hit == False):
+                bolt1.displayTarget()
+            # pygame.draw.rect(screen, Colors.muted_gray, (30, 195, 1220, 495), 2)
+            #  //////////////////////////////////////////// DISPLAY SHOTS ////////////////////////////////////////////
+            for bulletHoleP1 in player1.bulletHoles:
+                bulletHoleP1.displayImage()
+
+            for bulletHoleP2 in player2.bulletHoles:
+                bulletHoleP2.displayImage()
+            display_GUI_STATIC()
+            display_GUI_UPDATE(player1.bullets, player1.score, player2.bullets, player2.score)
+        # ///////////////////////////////////////// END OF ELSE ///////////////////////////////////////////////
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            # USER USED A KEY
+            elif (event.type == pygame.KEYDOWN):
+                if (player1.time > 0):
+                    # if (event.key == pygame.K_e):
+                    #     bolt1.effect()
+                    # ///////////////////////////////////////// SPACE (PLAYER 1 SHOOTING) /////////////////////////////////////////
+                    if (event.key == pygame.K_SPACE):
+                        player1.shoot(1)
+                    # ///////////////////////////////////////// PLAYER 1 MOVEMENTS /////////////////////////////////////////
+                    if (event.key == pygame.K_w):
+                        player1.moveUp()
+                    if (event.key == pygame.K_s):
+                        player1.moveDown()
+                    if (event.key == pygame.K_a):
+                        player1.moveLeft()
+                    if (event.key == pygame.K_d):
+                        player1.moveRight()
+                if (player2.time > 0):
+                    # ///////////////////////////////////////// ENTER (PLAYER 2 SHOOTING) /////////////////////////////////////////
+                    if (event.key == pygame.K_RETURN):
+                        player2.shoot(2)
+                    # ///////////////////////////////////////// PLAYER 2 MOVEMENTS /////////////////////////////////////////
+                    if (event.key == pygame.K_UP):
+                        player2.moveUp()
+                    if (event.key == pygame.K_DOWN):
+                        player2.moveDown()
+                    if (event.key == pygame.K_LEFT):
+                        player2.moveLeft()
+                    if (event.key == pygame.K_RIGHT):
+                        player2.moveRight()
+            # //////////////////////////////////////////// CALL CUSTOM EVENTS ////////////////////////////////////////////
+            elif (event.type == ammo_spawn):
+                ammo1.reset()
+                ammo2.reset()
+            elif (event.type == time_spawn):
+                extra_time1.reset()
+            elif (event.type == bomb_spawn):
+                bomb1.reset()
+        pygame.display.flip()
+
+    pygame.quit()
---
+++ CShot executable/menu.py
@@ -0,0 +1,172 @@
+from pygame import mixer
+import pygame, sys, subprocess
+import pygame.freetype
+
+
+class Colors:
+    # Helper class for colors
+    white = (255, 255, 255)
+    gray = (168, 168, 168)
+    dark_gray = (36, 36, 36)
+    muted_gray = (124, 124, 124)
+    muted_red = (150, 50, 50)
+    muted_green = (50, 150, 50)
+    muted_blue = (50, 50, 150)
+
+
+class Texts:
+    # Helper class for displaying Texts
+    def __init__(self, text, posX, posY, color, font_size):
+        self.text = text
+        self.color = color
+        self.posX = posX
+        self.posY = posY
+        self.size = font_size
+
+    def displayText(self):
+        height, width = self.posY, self.posX
+        text_font = pygame.font.Font('assets/PressStart2P-Regular.ttf', self.size)
+        text_surface = text_font.render(self.text, True, self.color)
+        text_rect = text_surface.get_rect(center=(width, height))
+        screen.blit(text_surface, text_rect)
+
+
+class Buttons:
+    # Helper class for displaying Buttons
+    def __init__(self, text, posX, posY, width, height, background_color, hover_color, text_color, text_size,
+                 action=None):
+        self.text = text
+        self.posX = posX
+        self.posY = posY
+        self.width = width
+        self.height = height
+        self.background_color = background_color
+        self.text_color = text_color
+        self.h_color = hover_color
+        self.font_size = text_size
+        self.action = action
+
+    def displayButton(self):
+        content = Texts(self.text, self.posX, self.posY, self.text_color, self.font_size)
+        width = self.width
+        height = self.height
+        x = self.posX - width // 2
+        y = self.posY - height // 2
+        h_color = self.h_color
+        background_color = self.background_color
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if (x < mouse[0] < x + width and y < mouse[1] < y + height):
+            pygame.draw.rect(screen, h_color, (x, y, width, height), border_radius=2)
+            if click[0] == 1 and self.action:
+                self.action()
+        else:
+            pygame.draw.rect(screen, background_color, (x, y, width, height), border_radius=2)
+
+        content.displayText()
+
+
+class GameState:
+    MENU = 0
+    CREDITS = 1
+
+
+class Actions:
+    @staticmethod
+    def exit_game():
+        pygame.quit()
+        sys.exit()
+
+    @staticmethod
+    def start_game():
+        subprocess.Popen(["python", "login.py"])
+        pygame.quit()
+        sys.exit()
+
+    @staticmethod
+    def show_credits():
+        global current_state
+        current_state = GameState.CREDITS
+
+    @staticmethod
+    def back_to_menu():
+        global current_state
+        current_state = GameState.MENU
+
+
+class Images:
+    def __init__(self, size, posX, posY, src):
+        self.size = size
+        self.posX = posX
+        self.posY = posY
+        self.src = f"assets/{src}"
+
+    def displayImage(self):
+        emoji_img = pygame.image.load(self.src)
+        emoji_img = pygame.transform.smoothscale(emoji_img, (self.size, self.size))
+        screen.blit(emoji_img, (self.posX, self.posY))
+
+
+# //////////////////////////////////////////// ASSETS INITIALIZE ////////////////////////////////////////////
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption('Menu')
+    pygame.font.init()
+    icon = pygame.image.load('assets/icon.jpg')
+    pygame.display.set_icon(icon)
+    font = pygame.font.Font('assets/PressStart2P-Regular.ttf', 50)
+    mixer.music.load('assets/menu.sf.mp3')
+    mixer.music.play(-1)
+    WIDTH, HEIGHT = 1280, 720
+    bg_img = pygame.image.load('assets/bg_empty.png')
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    current_state = GameState.MENU
+
+    # //////////////////////////////////////////// MAIN DRIVER CODE ////////////////////////////////////////////
+    while running:
+        clock.tick(144)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(Colors.dark_gray)
+        screen.blit(bg_img, (0, 0))
+        if current_state == GameState.MENU:
+            gameName = Texts('C', 560, 172, Colors.muted_red, 210)
+            gameName.displayText()
+            gameName = Texts('Shot', 677, 162, Colors.white, 75)
+            gameName.displayText()
+            gameName_emoji = Images(118, WIDTH // 2 + 33, 94, 'logo.png')
+            gameName_emoji.displayImage()
+            startButton = Buttons("Start", WIDTH // 2, 345, 320, 90, Colors.muted_gray, Colors.gray, Colors.dark_gray,
+                                  48,
+                                  Actions.start_game)
+            creditsButton = Buttons("Credits", WIDTH // 2, 450, 305, 70, Colors.muted_gray, Colors.gray,
+                                    Colors.dark_gray, 36,
+                                    Actions.show_credits)
+            exitButton = Buttons("Exit", WIDTH // 2, 545, 210, 70, Colors.dark_gray, Colors.dark_gray,
+                                 Colors.muted_gray,
+                                 36,
+                                 Actions.exit_game)
+            startButton.displayButton()
+            creditsButton.displayButton()
+            exitButton.displayButton()
+        elif current_state == GameState.CREDITS:
+            backButton = Buttons("Back", WIDTH // 2, 650, 210, 70, Colors.muted_gray, Colors.gray, Colors.dark_gray, 36,
+                                 Actions.back_to_menu)
+            creditsTitle = Texts("Credits", WIDTH // 2, 100, Colors.white, 60)
+            creditsTitle.displayText()
+            credit1 = Texts("Designed by:", WIDTH // 2, 250, Colors.muted_gray, 30)
+            credit2 = Texts("Sina Fatemi", WIDTH // 2, 300, Colors.muted_gray, 30)
+            credit3 = Texts("Amir Sajedi", WIDTH // 2, 350, Colors.muted_gray, 30)
+            credit4 = Texts("IUST AP Project of 2025", WIDTH // 2, 400, Colors.muted_gray, 30)
+            credit1.displayText()
+            credit2.displayText()
+            credit3.displayText()
+            credit4.displayText()
+            backButton.displayButton()
+        pygame.display.flip()
+    pygame.quit()
